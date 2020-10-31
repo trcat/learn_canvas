45b6e1d9327146f2fcc931b13be0d9db3c46a745
DayDay <daydaybigking@sina.com>
2020-10-31 10:31:32 +0800

    完成下雪特效

--- src/snow.js
+++ src/snow.js
@@ -2,15 +2,19 @@ const canvas = document.getElementById("canvas");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 canvas.style.position = "fixed";
-let ctx;
+let ctx,
+  count = 0;
 const list = [];
 
-class Start {
-  constructor(_canvas) {
-    this.x = _canvas.width - Math.floor(Math.random() * _canvas.width);
-    this.y = _canvas.height - Math.floor(Math.random() * _canvas.height);
-    this.size = Math.random() * 4 + 2;
-    this.speed = 1;
+class Snow {
+  constructor(_canvas, x, y) {
+    this.x = x || _canvas.width - Math.floor(Math.random() * _canvas.width);
+    this.y =
+      y === null || typeof y === "undefined"
+        ? _canvas.height - Math.floor(Math.random() * _canvas.height)
+        : y;
+    this.size = Math.random() * 2 + 1;
+    this.speed = 0.5;
     this.canvas = _canvas;
     this.radian = null;
   }
@@ -19,40 +23,49 @@ class Start {
     _ctx.save();
     _ctx.translate(this.x, this.y);
     _ctx.beginPath();
-    _ctx.moveTo(this.size, 0);
-    for (let i = 0; i < 9; i++) {
-      _ctx.rotate(Math.PI / 5);
-      if (i % 2 == 0) {
-        _ctx.lineTo((this.size / 0.525731) * 0.200811, 0);
-      } else {
-        _ctx.lineTo(this.size, 0);
-      }
-    }
-    _ctx.closePath();
+    _ctx.arc(
+      this.x + this.size,
+      this.y + this.size,
+      this.size,
+      0,
+      Math.PI * 2,
+      true
+    );
     _ctx.fillStyle = "#fff";
     _ctx.fill();
     _ctx.restore();
   }
   computedPosition() {
-    !this.radian && (this.radian = (Math.random() * Math.PI) / 2);
-    if (this.x >= this.canvas.width / 2) {
-      this.x += Math.abs(Math.cos(this.radian) * this.speed);
-    } else {
-      this.x -= Math.abs(Math.cos(this.radian) * this.speed);
-    }
-    this.y += Math.abs(Math.sin(this.radian) * this.speed);
+    !this.radian && (this.radian = Math.random() * Math.PI);
+    this.x += Math.abs(Math.cos(this.radian) * this.speed);
+    this.y += this.speed;
   }
 }
 
 function init() {
   ctx = canvas.getContext("2d");
-  for (let i = 0; i < 250; i++) {
-    list.push(new Start(canvas));
-  }
+  generateSnow()
   requestAnimationFrame(draw);
 }
 
+function generateSnow() {
+  for (let i = 0; i < 100; i++) {
+    list.push(
+      new Snow(
+        canvas,
+        canvas.width - Math.floor(Math.random() * canvas.width),
+        Math.floor(Math.random() * canvas.height * 0.5) * -1
+      )
+    );
+  }
+}
+
 function draw() {
+  if (count === 50) {
+    generateSnow()
+    count = 0;
+  }
+  count += 1;
   const linearColor = ctx.createLinearGradient(0, 0, 0, canvas.height);
   linearColor.addColorStop(0, "#232256");
   linearColor.addColorStop(1, "#143778");
